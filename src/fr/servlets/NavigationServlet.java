243ae85947a1307ed86279895ee7b7ee21a8b2b1
MToussaint <toussaintmad@wanadoo.fr>
2016-11-30 19:56:05 +0100

    premier jet de ma servlet

---
+++ src/fr/servlets/NavigationServlet.java
@@ -0,0 +1,29 @@
+package fr.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class NavigationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public NavigationServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.getServletContext().getRequestDispatcher("/zoo.jsp").forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
