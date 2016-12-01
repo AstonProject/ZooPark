d6f0290a74f8e7640023b29e779d2b54c5278374
Latverius <simeon_guillaume@yahoo.fr>
2016-12-01 09:14:23 +0100

    Update NavigationServlet.java
    J'ai supprimé la ligne par défaut du doGet "Served at..." et le numéro de version servlet (inutile) sinon c'est ok je valide !

--- src/fr/servlets/NavigationServlet.java
+++ src/fr/servlets/NavigationServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class NavigationServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
        
    
     public NavigationServlet() {
@@ -17,7 +16,6 @@ public class NavigationServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 		this.getServletContext().getRequestDispatcher("/zoo.jsp").forward(request, response);
 	}
 
