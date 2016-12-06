e804f9b1e8bfed377e2ec916fb89777798d9ac33
MToussaint <toussaintmad@wanadoo.fr>
2016-12-06 17:08:38 +0100

    bean faits

---
+++ src/fr/beans/AnimalBean.java
@@ -0,0 +1,72 @@
+package fr.beans;
+
+public class AnimalBean {
+	
+	private int id;
+	private String name;
+	private int hungry_gauge;
+	private int health_gauge;
+	private String description;
+	private int specie_id;
+	private int enclosure_id;
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getHungry_gauge() {
+		return hungry_gauge;
+	}
+	
+	public void setHungry_gauge(int hungry_gauge) {
+		this.hungry_gauge = hungry_gauge;
+	}
+	
+	public int getHealth_gauge() {
+		return health_gauge;
+	}
+	
+	public void setHealth_gauge(int health_gauge) {
+		this.health_gauge = health_gauge;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getSpecie_id() {
+		return specie_id;
+	}
+	
+	public void setSpecie_id(int specie_id) {
+		this.specie_id = specie_id;
+	}
+	
+	public int getEnclosure_id() {
+		return enclosure_id;
+	}
+	
+	public void setEnclosure_id(int enclosure_id) {
+		this.enclosure_id = enclosure_id;
+	}
+	
+	
+
+}
---
+++ src/fr/beans/EnclosureBean.java
@@ -0,0 +1,99 @@
+package fr.beans;
+
+public class EnclosureBean {
+	
+	private int id;
+	private int locate_x;
+	private int locate_y;
+	private int capacity;
+	private String description;
+	private int animal_quantity;
+	private int cleanliness_gauge;
+	private int employee_slot;
+	private int employee_quantity;
+	private int specie;
+	
+	
+		public int getId() {
+		return id;
+	}
+		
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getLocate_x() {
+		return locate_x;
+	}
+	
+	public void setLocate_x(int locate_x) {
+		this.locate_x = locate_x;
+	}
+	
+	public int getLocate_y() {
+		return locate_y;
+	}
+	
+	public void setLocate_y(int locate_y) {
+		this.locate_y = locate_y;
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getAnimal_quantity() {
+		return animal_quantity;
+	}
+	
+	public void setAnimal_quantity(int animal_quantity) {
+		this.animal_quantity = animal_quantity;
+	}
+	
+	public int getCleanliness_gauge() {
+		return cleanliness_gauge;
+	}
+	
+	public void setCleanliness_gauge(int cleanliness_gauge) {
+		this.cleanliness_gauge = cleanliness_gauge;
+	}
+	
+	public int getEmployee_slot() {
+		return employee_slot;
+	}
+	
+	public void setEmployee_slot(int employee_slot) {
+		this.employee_slot = employee_slot;
+	}
+	
+	public int getEmployee_quantity() {
+		return employee_quantity;
+	}
+	
+	public void setEmployee_quantity(int employee_quantity) {
+		this.employee_quantity = employee_quantity;
+	}
+	
+	public int getSpecie() {
+		return specie;
+	}
+	
+	public void setSpecie(int specie) {
+		this.specie = specie;
+	}
+	
+	
+
+}
