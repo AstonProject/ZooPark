c5b4930c7dca77ce671324d40f6623f97eefcc18
MToussaint <toussaintmad@wanadoo.fr>
2016-12-08 10:24:01 +0100

    classe connection faite

---
+++ src/fr/utility/ConnectionBDD.java
@@ -0,0 +1,52 @@
+package fr.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionBDD {
+	private static Connection connection;
+
+	static String driver = "com.mysql.jdbc.Driver";
+	static String url = "jdbc:mysql://localhost:3306/zoopark?user=root&password=root";
+	static String user = "root";
+	static String password = "root";
+
+	static {
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public static Connection getConnection() {
+		return connection;
+	}
+
+	public static void disconnection(ResultSet result, Statement statement) {
+		if (result != null) {
+			try {
+				result.close();
+			} catch (SQLException ignore) {
+			}
+		}
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException ignore) {
+			}
+		}
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException ignore) {
+			}
+		}
+	}
+}
