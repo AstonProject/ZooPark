858eff8792fd6980680d09b9839f3963df2fd857
Latverius <simeon_guillaume@yahoo.fr>
2016-12-14 17:32:02 +0100

    modifs CostsDAO

--- src/fr/dao/CostsDAO.java
+++ src/fr/dao/CostsDAO.java
@@ -1,6 +1,8 @@
 package fr.dao;
 
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -8,8 +10,9 @@ import org.json.simple.parser.JSONParser;
 public class CostsDAO {
 
 	@SuppressWarnings("unchecked")
-	public static void getCosts() {
+	public static Map<String,String> getCosts() {
 		JSONParser parser = new JSONParser();
+		Map<String, String> costs = new HashMap<>();
 
 		try {
 
@@ -18,25 +21,24 @@ public class CostsDAO {
 			JSONObject jsonObject = (JSONObject) obj;
 
 			String enclosureCosts_lion = (String) jsonObject.get("enclosureCosts_lion");
+			costs.put("enclosureCosts_lion", enclosureCosts_lion);
 			String enclosureCosts_giraffe = (String) jsonObject.get("enclosureCosts_giraffe");
+			costs.put("enclosureCosts_giraffe", enclosureCosts_giraffe);
 			String enclosureCosts_elephant = (String) jsonObject.get("enclosureCosts_elephant");
+			costs.put("enclosureCosts_elephant", enclosureCosts_elephant);
 			String enclosureCosts_camel = (String) jsonObject.get("enclosureCosts_camel");
+			costs.put("enclosureCosts_camel", enclosureCosts_camel);
 			String lionCosts = (String) jsonObject.get("lionCosts");
+			costs.put("lionCosts", lionCosts);
 			String giraffeCosts = (String) jsonObject.get("giraffeCosts");
+			costs.put("giraffeCosts", giraffeCosts);
 			String elephantCosts = (String) jsonObject.get("elephantCosts");
+			costs.put("elephantCosts", elephantCosts);
 			String camelCosts = (String) jsonObject.get("camelCosts");
-
-			System.out.println("Enclosure costs for a lion: " + enclosureCosts_lion);
-			System.out.println("Enclosure costs for a giraffe: " + enclosureCosts_giraffe);
-			System.out.println("Enclosure costs for an elephant: " + enclosureCosts_elephant);
-			System.out.println("Enclosure costs for a camel: " + enclosureCosts_camel);
-			System.out.println("Costs for a lion: " + lionCosts);
-			System.out.println("Costs for a giraffe: " + giraffeCosts);
-			System.out.println("Costs for an elephant: " + elephantCosts);
-			System.out.println("Costs for a camel: " + camelCosts);
-
+			costs.put("camelCosts", camelCosts);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return costs;
 	}
 }
