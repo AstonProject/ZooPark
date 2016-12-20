399cca1488ede8c1752a9fdab84f81c1d292aa24
Latverius <simeon_guillaume@yahoo.fr>
2016-12-20 10:59:57 +0100

    modifs système home

--- src/fr/servlets/Home.java
+++ src/fr/servlets/Home.java
@@ -1,6 +1,8 @@
 package fr.servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import fr.dao.CostsDAO;
+import fr.beans.EnclosureBean;
+import fr.beans.PlayerBean;
+import fr.dao.EnclosuresDAO;
 
 @WebServlet("/home")
 public class Home extends HttpServlet {
@@ -20,6 +24,11 @@ public class Home extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
+		PlayerBean user = (PlayerBean) session.getAttribute("user");
+		/*List<EnclosureBean> enclos = null;
+		EnclosuresDAO edao = new EnclosuresDAO();
+		enclos = edao.getAllEnclosures(user.getId());
+		session.setAttribute("construction", enclos);*/
 		this.getServletContext()
 			.getRequestDispatcher("/WEB-INF/home.jsp")
 			.forward(request, response);
